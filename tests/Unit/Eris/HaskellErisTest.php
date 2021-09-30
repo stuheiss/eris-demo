5c43a7820222571d78c0b17674dc6eeda31404f7
Stu Heiss <sheiss@highlandsolutions.com>
2021-09-30 13:50:27 -0500

    Add silly encode/decode test

--- tests/Unit/Eris/HaskellErisTest.php
+++ tests/Unit/Eris/HaskellErisTest.php
@@ -4,7 +4,7 @@ use Eris\Generator;
 use PHPUnit\Framework\TestCase;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithExceptionHandling;
 
-class HaskellTest extends TestCase
+class HaskellErisTest extends TestCase
 {
     use Eris\TestTrait;
     use Tests\GetAnnotations;
@@ -78,6 +78,27 @@ class HaskellTest extends TestCase
                 $expected = $array;
                 sort($array);
                 $this->assertEquals($expected, $array);
+            });
+    }
+
+    /**
+     * @test
+     * @eris-repeat 100
+     *
+     * Property: decoding an encoded string equals the original string
+     */
+    public function testDecodeEncodedStringReturnsOriginalString()
+    {
+        // $this->withoutExceptionHandling();
+        $this
+            ->forAll(
+                Generator\string()
+            )
+            ->then(function ($str) {
+                $encoded = base64_encode($str);
+                $expected = base64_decode($encoded);
+                // var_dump(['original'=>$str, 'encoded'=>$encoded]);
+                $this->assertEquals($expected, $str);
             });
     }
 }
